4924398206017c990837dd13c97fb3e699f6d755
alex <a.korneyko@gmail.com>
2020-07-06 13:34:58 +0200

    Methods GET and POST

---
+++ HttpResource/HttpClient.cs
@@ -0,0 +1,58 @@
+﻿using System.Net;
+using System.Threading;
+
+namespace HttpResource
+{
+    public delegate void ResponseHandler<T>(T resultObject, HttpStatusCode httpStatusCode);
+    
+    public class HttpClient
+    {
+        public string BearerToken { get; private set; }
+        public bool IsAuthenticated { get; private set; }
+
+        public HttpClient() { }
+
+        public HttpClient(string loginApiUrl, string username, string password)
+        {
+            Authenticate(loginApiUrl, username, password);
+        }
+
+        public bool Authenticate(string apiUrl, string username, string password)
+        {
+            var requestObject = new {Username = username, Password = password};
+            Post<string>(apiUrl, requestObject, (token, statusCode) =>
+            {
+                if (statusCode != HttpStatusCode.OK || token == "") return;
+                BearerToken = token;
+                IsAuthenticated = true;
+            });
+            
+            return IsAuthenticated;
+        }
+
+//----------------------------------------------------------------------------------------------------------------------
+
+        public void Get<T>(string url, ResponseHandler<T> responseHandler) where T : class
+        {
+            var requestHandler = new RequestHandler<T>(url, null, responseHandler);
+
+            var thread = new Thread(requestHandler.GetRequest);
+            thread.Start();
+        }
+        
+
+        public void Post<T>(string url, ResponseHandler<T> responseHandler) where T : class
+        {
+            Post(url, new object(), responseHandler);
+        }
+        
+//----------------------------------------------------------------------------------------------------------------------
+        public void Post<T>(string url, object requestObject, ResponseHandler<T> responseHandler) where T : class
+        {
+            var requestHandler = new RequestHandler<T>(url, requestObject, responseHandler) {BearerToken = BearerToken};
+
+            var thread = new Thread(requestHandler.PostRequest);
+            thread.Start();
+        }
+    }
+}
---
+++ HttpResource/RequestHandler.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text.Json;
+
+namespace HttpResource
+{
+    class RequestHandler<T> where T : class
+    {
+        public string BearerToken { get; set; }
+        
+        private readonly ResponseHandler<T> _responseHandler;
+        private readonly string _url;
+        private readonly object _requestObject;
+
+        public RequestHandler(string url, object requestObject, ResponseHandler<T> responseHandler)
+        {
+            _responseHandler = responseHandler;
+            _url = url;
+            _requestObject = requestObject;
+        }
+
+//----------------------------------------------------------------------------------------------------------------------
+        public void GetRequest()
+        {
+            var webRequest = (HttpWebRequest)WebRequest.Create(_url);
+            webRequest.ContentType = "application/json";
+            webRequest.Method = "GET";
+            webRequest.Headers.Add(HttpRequestHeader.Authorization, $"Bearer {BearerToken}");
+            
+            HttpWebResponse webResponse;
+            try
+            {
+                webResponse = (HttpWebResponse) webRequest.GetResponse();
+            }
+            catch (WebException e)
+            {
+                var httpStatusCode = ((HttpWebResponse)e.Response).StatusCode;
+                _responseHandler.Invoke(null, httpStatusCode);
+                return;
+            }
+
+            var resultObject = GetResultObject(webResponse);
+            
+            _responseHandler.Invoke(resultObject, webResponse.StatusCode);
+        }
+
+//----------------------------------------------------------------------------------------------------------------------
+        public void PostRequest()
+        {
+            var webRequest = (HttpWebRequest)WebRequest.Create(_url);
+            webRequest.ContentType = "application/json";
+            webRequest.Method = "POST";
+            webRequest.Headers.Add(HttpRequestHeader.Authorization, $"Bearer {BearerToken}");
+            
+            using (var streamWriter = new StreamWriter(webRequest.GetRequestStream()))
+            {
+                var json = JsonSerializer.Serialize(_requestObject);
+                streamWriter.Write(json);
+            }
+            
+            HttpWebResponse webResponse;
+            try
+            {
+                webResponse = (HttpWebResponse) webRequest.GetResponse();
+            }
+            catch (WebException e)
+            {
+                var httpStatusCode = ((HttpWebResponse)e.Response).StatusCode;
+                _responseHandler.Invoke(null, httpStatusCode);
+                return;
+            }
+
+            var resultObject = GetResultObject(webResponse);
+            
+            _responseHandler.Invoke(resultObject, webResponse.StatusCode);
+        }
+
+//----------------------------------------------------------------------------------------------------------------------
+        private static T GetResultObject(WebResponse webResponse)
+        {
+            T resultObject;
+            
+            using (var streamReader =
+                new StreamReader(webResponse.GetResponseStream() ?? throw new InvalidOperationException()))
+            {
+                var stringResponse = streamReader.ReadToEnd();
+                var options = new JsonSerializerOptions {PropertyNameCaseInsensitive = true};
+
+                resultObject = stringResponse != "" ? JsonSerializer.Deserialize<T>(stringResponse, options) : null;
+            }
+
+            return resultObject;
+        }
+    }
+}
